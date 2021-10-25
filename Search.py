4a9710d86095c9e16c4d968328650c332dfca421
Anton Mamyko <anton.mamyko2@gmail.com>
2021-10-26 11:53:58 +1300

    seach algo

---
+++ Search.py
@@ -0,0 +1,29 @@
+
+import random
+def midf(upper, lower):
+   return int(round((upper - lower) / 2, 0))
+
+lower = 0
+upper = 9999999999
+q = False
+think = random.randrange(lower, upper)
+
+mid = midf(upper, lower)
+print(mid)
+
+while q == False:
+
+    if mid < think:
+        print ('{} is too low'.format(mid))
+        lower = mid
+        mid = lower + int(round((upper - lower) / 2, 0))
+
+    elif mid > think:
+        print ('{} is too high'.format(mid))
+        upper = mid
+        mid = int(round((upper - lower) / 2, 0))
+    elif mid == think:
+        print(' Correct! I guessed {} correctly, I went for {}'.format(mid, think))
+        q = True
+
+
