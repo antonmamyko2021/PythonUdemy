b9e6a01d96744fef6e7b078306e6c7b79d4b7ca9
Anton Mamyko <anton.mamyko2@gmail.com>
2021-10-26 08:11:30 +1300

    addign search dev

--- P1.py
+++ P1.py
@@ -1,2 +1,10 @@
-splitString = "this string has been \n split \n several \n times"
-print(splitString)
+mult1 = range(0,11,1)
+mult2 = range(0,11,1)
+
+for i in mult1:
+    for j in mult2:
+        print('\t{} by {} is {}'.format(i, j, i*j))
+
+    print("*" * 50)
+
+
